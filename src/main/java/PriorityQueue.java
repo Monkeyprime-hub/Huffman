19a10237493304bb3bd9bc4c59dba2287ebf99dd
Vladislav Dudko <vladyslav.dudko@nure.ua>
2019-12-27 20:14:25 +0200

    Add Node, BinaryTree and PriorityQueue.

---
+++ src/main/java/PriorityQueue.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+class PriorityQueue {
+    private ArrayList<BinaryTree> data;
+    private int nElems;
+
+    public PriorityQueue() {
+        data = new ArrayList<BinaryTree>();
+        nElems = 0;
+    }
+
+    public void insert(BinaryTree newTree) {
+        if (nElems == 0)
+            data.add(newTree);
+        else {
+            for (int i = 0; i < nElems; i++) {
+                if (data.get(i).getFrequence() > newTree.getFrequence()) {
+                    data.add(i, newTree);
+                    break;
+                }
+                if (i == nElems - 1)
+                    data.add(newTree);
+            }
+        }
+        nElems++;
+    }
+
+    public BinaryTree remove() {
+        BinaryTree tmp = data.get(0);
+        data.remove(0);
+        nElems--;
+        return tmp;
+    }
+}
